c6ac4417a08fab53ca0252709a1d38c391d0cd75
MinhHieuUnity <minhhieuitfly@gmail.com>
2020-08-01 20:48:09 +0700

    Update Test.cs

--- FPSGitTest4/Assets/Script/Test.cs
+++ FPSGitTest4/Assets/Script/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.LogError("Hi");
     }
 
     // Update is called once per frame
