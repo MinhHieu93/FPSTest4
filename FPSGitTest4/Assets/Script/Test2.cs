c9ee2b38288933a16f1b7aa61550e6460ed06382
MinhHieuUnity <minhhieuitfly@gmail.com>
2020-08-01 20:50:29 +0700

    *+ Update Test1 and Add Test 2

--- FPSGitTest4/Assets/Script/Test2.cs
+++ FPSGitTest4/Assets/Script/Test2.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogError("Hi test1");
+        Debug.LogError("Hi test2");
     }
 
     // Update is called once per frame
